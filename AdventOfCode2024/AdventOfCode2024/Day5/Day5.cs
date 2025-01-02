f5962f4209081b811db9eee91d7e997ffd80dfa5
Corentin LE GALL (ANEO) <clegall@aneo.fr>
2025-01-02 17:41:39 +0100

    day 5 part1

---
+++ AdventOfCode2024/AdventOfCode2024/Day5/Day5.cs
@@ -0,0 +1,89 @@
+﻿namespace AdventOfCode2024.Day5
+{
+    public class Day5
+    {
+        private async Task<string[]> ReadLinesAsync(StreamReader sr)
+        {
+            var line = await sr.ReadToEndAsync();
+            var content = line.Replace("\r\n", "\n").Split("\n\n");
+            return content;
+            
+        }
+
+        public async Task<int> SolutionPart1Async(StreamReader sr)
+        {
+            var fileContent = await ReadLinesAsync(sr);
+            var pageOrderingRules = fileContent[0].Split("\n");
+            var PageUpdates = fileContent[1].Split("\n");
+            Dictionary<int, HashSet<int>> orderingRulesDictBeforeKey = new Dictionary<int, HashSet<int>>();
+            Dictionary<int, HashSet<int>> orderingRulesDictAfterKey = new Dictionary<int, HashSet<int>>();
+            foreach(var rule in pageOrderingRules)
+            {
+                string[] ruleContent = rule.Split("|");
+                var pagePrintedFirst = ruleContent[0];
+                var pagePrintedLast = ruleContent[1];
+                if(!int.TryParse(pagePrintedFirst,out int firstPage) || !int.TryParse(pagePrintedLast, out int lastPage))
+                {
+                    continue;
+                }
+                if (!orderingRulesDictBeforeKey.ContainsKey(firstPage))
+                {
+                    orderingRulesDictBeforeKey.Add(firstPage, new HashSet<int>() { lastPage});
+                }
+                if (!orderingRulesDictAfterKey.ContainsKey(lastPage))
+                {
+                    orderingRulesDictAfterKey.Add(lastPage, new HashSet<int>() { firstPage });
+                }
+                orderingRulesDictBeforeKey[firstPage].Add(lastPage);
+                orderingRulesDictAfterKey[lastPage].Add(firstPage);
+            }
+            var updates = PageUpdates
+                            .Select(s => s.Split(",").Select(v => int.Parse(v.ToString())).ToArray())
+                            .ToArray();
+
+            var wellOrderedUpdates = new List<int[]>();
+            foreach(var update in updates)
+            {
+                bool isOrdered = true;
+                for(int i = 0; i<update.Length; i++)
+                {
+                    if (!orderingRulesDictBeforeKey.ContainsKey(update[i]))
+                    {
+                        continue;
+                    }
+                    var nextPages = update
+                        .Where((p, idx) => idx > i && !orderingRulesDictBeforeKey[update[i]].Contains(p))
+                        .ToArray();
+                    var beforePages = update
+                        .Where((p, idx) => idx < i && !orderingRulesDictAfterKey[update[i]].Contains(p))
+                        .ToArray();
+                    if (nextPages.Any() || beforePages.Any())
+                    {
+                        isOrdered = false;
+                        break;
+                    }
+                }
+                if (isOrdered)
+                {
+                    wellOrderedUpdates.Add(update);
+                }
+            }
+            int count = 0;
+            foreach (var wellOrderedUpdate in wellOrderedUpdates)
+            {
+                var res = FindMiddleElement(wellOrderedUpdate);
+                count += res;
+            }
+            return count;
+        }
+
+        private int FindMiddleElement(int[] array)
+        {
+            if(array.Length%2 == 0)
+            {
+                return array[array.Length/2];
+            }
+            return array[array.Length/2];
+        }
+    }
+}
--- AdventOfCode2024/AdventOfCode2024/Program.cs
+++ AdventOfCode2024/AdventOfCode2024/Program.cs
@@ -2,17 +2,18 @@
 using AdventOfCode2024.Day2;
 using AdventOfCode2024.Day3;
 using AdventOfCode2024.Day4;
+using AdventOfCode2024.Day5;
 using System.Diagnostics;
 class Program
 {
     static async Task Main(string[] args)
     {
-        string day = "Day4";
+        string day = "Day5";
         StreamReader sr = new StreamReader($"../../../{day}/input.txt");
-        Day4 day4 = new Day4();
+        Day5 day5 = new Day5();
         var stopWatch = new Stopwatch();
         stopWatch.Start();
-        var res = await day4.SolutionPart2Async(sr);
+        var res = await day5.SolutionPart1Async(sr);
         stopWatch.Stop();
         Console.WriteLine(res);
         Console.WriteLine($"Execution Time = {stopWatch.Elapsed}");
